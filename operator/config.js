0cbdd8623cd4b8891d623456f515d46aada9912b
SCOdad <166066758+SCOdad@users.noreply.github.com>
2026-08-23 14:45:46 -0400

    BKLG-0110 configure analytics endpoint

--- operator/config.js
+++ operator/config.js
@@ -2,3 +2,4 @@
 window.DV_OPERATOR_BACKLOG_ENDPOINT='https://cayoyqwrmouxuttloemc.supabase.co/functions/v1/operator-backlog';
 window.DV_OPERATOR_FEEDBACK_ENDPOINT='https://cayoyqwrmouxuttloemc.supabase.co/functions/v1/operator-feedback';
 window.DV_OPERATOR_PRODUCT_SIGNALS_ENDPOINT='https://cayoyqwrmouxuttloemc.supabase.co/functions/v1/operator-product-signals';
+window.DV_OPERATOR_ANALYTICS_ENDPOINT='https://cayoyqwrmouxuttloemc.supabase.co/functions/v1/operator-analytics';
