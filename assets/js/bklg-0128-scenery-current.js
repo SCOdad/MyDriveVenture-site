6d9f6809b49a4c6d5908dcb9dbfdcd69bbc77abb
SCOdad <166066758+SCOdad@users.noreply.github.com>
2026-09-10 09:48:34 -0400

    BKLG-0128: use current unversioned scenery assets

---
+++ assets/js/bklg-0128-scenery-current.js
@@ -0,0 +1,36 @@
+(() => {
+  const CURRENT = [
+    {value:'school',sortKey:'03-BACKGROUND-SCHOOL',id:'L2-DRAFT',name:'School Background',file:'DV03-L2-SCHOOL-BACKGROUND-DRAFT.png'},
+    {value:'grocery-store',sortKey:'03-BACKGROUND-GROCERY-STORE',id:'L4-DRAFT',name:'Grocery Store Background',file:'DV03-L4-GROCERY-STORE-BACKGROUND-DRAFT.png'},
+    {value:'library',sortKey:'03-BACKGROUND-LIBRARY',id:'L5-DRAFT',name:'Library Background',file:'DV03-L5-LIBRARY-BACKGROUND-DRAFT.png'},
+    {value:'snack-run',sortKey:'03-BACKGROUND-SNACK-RUN',id:'L6-DRAFT',name:'Snack Run Background',file:'DV03-L6-SNACK-RUN-BACKGROUND-DRAFT.png'},
+    {value:'car-wash',sortKey:'03-BACKGROUND-CAR-WASH',id:'L7-DRAFT',name:'Car Wash Background',file:'DV03-L7-CAR-WASH-BACKGROUND-DRAFT.png'},
+    {value:'gas-station',sortKey:'03-BACKGROUND-GAS-STATION',id:'L8-DRAFT',name:'Gas Station Background',file:'DV03-L8-GAS-STATION-BACKGROUND-DRAFT.png'}
+  ];
+  const srcFor=item=>`/assets/images/dv03/layers/${item.file}`;
+
+  if(document.documentElement.dataset.dvRoute==='bklg0128uat'){
+    const apply=()=>{
+      const api=window.DV_BKLG_0128_UAT;
+      const background=api?.LAYERS?.find(layer=>layer.id==='background');
+      if(!background)return false;
+      CURRENT.forEach(item=>{
+        const option=background.options.find(candidate=>candidate.value===item.value);
+        if(option){option.label=item.name.replace(' Background','');option.sortKey=item.sortKey;option.src=srcFor(item);option.draft=true;}
+      });
+      api.applyLayers?.();
+      return true;
+    };
+    if(!apply())setTimeout(apply,0);
+  }
+
+  if(document.documentElement.dataset.dvRoute==='bklg0128gallery'){
+    const api=window.DV_BKLG_0128_GALLERY;
+    if(api?.ASSETS){
+      CURRENT.forEach(item=>{
+        const asset=api.ASSETS.find(candidate=>candidate.sortKey===item.sortKey);
+        if(asset){asset.id=item.id;asset.name=item.name;asset.file=item.file;asset.src=srcFor(item);asset.status='Draft / UAT';}
+      });
+    }
+  }
+})();
