ee1bae2ed6f6c01b14bbfd5f0faf35ab85ff4e03
SCOdad <166066758+SCOdad@users.noreply.github.com>
2026-09-11 15:11:17 -0400

    BKLG-0128: cover revised Snack Run asset version

--- tests/bklg-0128-background-drafts.test.js
+++ tests/bklg-0128-background-drafts.test.js
@@ -47,6 +47,7 @@ test('BKLG-0128 Drive Thru is native to the authoritative Background harness',()
   assert.doesNotMatch(current,/createElement\('button'\)/);
   assert.doesNotMatch(current,/insertBefore\(button/);
   assert.match(current,/721c2d7-library/);
+  assert.match(current,/56aafef-snack-run/);
   assert.match(current,/721c2d7-drive-thru/);
   assert.match(current,/e83616b-car-wash/);
 });
